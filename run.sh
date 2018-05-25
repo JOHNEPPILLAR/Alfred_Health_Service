5a9551ca5816bb689b499a4f47304dfc9bcf0ac6
JOHNEPPILLAR <johneppillar@gmail.com>
2018-05-25 18:25:45 +0100

    removed layer in docker file

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ lsof -i :3981
 kill -9 $(lsof -sTCP:LISTEN -i:3981 -t)
 
 #echo "Removing node modules folder and installing latest"
-#rm -rf node_modules
-#npm install
+rm -rf node_modules
+npm install
 
 echo "Run the server"
 nodemon lib/server.js
