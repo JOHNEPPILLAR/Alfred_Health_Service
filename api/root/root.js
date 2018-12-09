632b387d090fb443d790118afee8a9bcdc64d134
JP <john.pillar@rbs.co.uk>
2018-12-09 13:47:02 +0000

    Refactored log and Alfred service calls

--- api/root/root.js
+++ api/root/root.js
@@ -156,7 +156,7 @@ async function healthCheck(req, res, next) {
       }
     });
   } catch (err) {
-    serviceHelper.log('error', 'healthCheck', err);
+    serviceHelper.log('error', 'healthCheck', err.message);
     serviceHelper.sendResponse(res, false, err);
     next();
   }
@@ -186,13 +186,9 @@ skill.get('/healthcheck', healthCheck);
  */
 async function reRegister(req, res, next) {
   serviceHelper.log('trace', 'reRegister', 'reRegister API called');
-
-  let returnMessage = 'Re-registered service';
-
-  if (!serviceHelper.registerService()) returnMessage = 'Unable to re-register service';
-
-  serviceHelper.log('trace', 'reRegister', returnMessage);
-  serviceHelper.sendResponse(res, false, returnMessage);
+  serviceHelper.log('trace', 'reRegister', 'Attempt to reRegister service');
+  serviceHelper.registerService();
+  serviceHelper.sendResponse(res, false, 'Attempt to reRegister service called');
   next();
 }
 skill.get('/reregister', reRegister);
