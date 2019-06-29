ff9069c88b7e8e20f1d7524ccf65ce5b656f2928
JOHNEPPILLAR <johneppillar@gmail.com>
2019-06-29 22:41:10 +0100

    Change controller ip

--- api/root/root.js
+++ api/root/root.js
@@ -129,7 +129,7 @@ async function healthCheck(req, res, next) {
     },
     {
       name: 'alfred_controller_service',
-      ip: 'alfred_controller_service',
+      ip: '192.168.1.3',
       port: 3981,
     },
     {
